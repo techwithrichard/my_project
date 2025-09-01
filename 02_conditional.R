fc274a2072593abadbcbfbbe6e155f900dda107e
techwithrichard <richardkoech69@gmail.com>
2025-09-01 20:20:23 +0300

    added if-conditional statement

---
+++ 02_conditional.R
@@ -0,0 +1,24 @@
+# CONDITIONAL STATEMENTS
+# If, else if, else => executed when the condition is True
+# switch -> select block o fcode when users input meets the criteria (number/characters)
+
+cat("conditional statements\n")
+
+# load data
+cat("Step 1- loading mtcars dataset\n")
+data(mtcars)
+
+cat("step 2 - display the first five recods")
+head(mtcars, 5)
+
+
+# first conditional statement - if
+cat("Step 3 - if statement\n")
+name <- "Mazda"
+if (name == "Mazda") {
+  cat("Corect name picked:  Mazda\n")
+}
+
+
+
+
