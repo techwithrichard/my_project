5701d54975be13a9b6a469c8bed67c4482781eb6
techwithrichard <richardkoech69@gmail.com>
2025-09-01 20:09:45 +0300

    sequential R code

---
+++ 01_sequential.R
@@ -0,0 +1,17 @@
+
+# SEQUENTIAL CODE EXECUTION
+
+cat("Step 1 - First statement to be executed ")
+
+cat("Step 2 - :Load libraries")
+
+cat("Step 3 - Import datatset")
+
+cat("Step 4 - Train the model")
+
+cat("Step 5 - Evaluate the model performance")
+
+cat("Step 6 - Plot or deploy the model")
+
+
+
